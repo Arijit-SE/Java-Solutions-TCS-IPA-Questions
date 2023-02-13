dd2b87cdf512e911b7ee6d65b312c17b41e8a247
ARIJIT GHOSH <112297809+arijitgh29@users.noreply.github.com>
2023-02-13 11:05:51 +0530

    Add files via upload
    Signed-off-by: ARIJIT GHOSH <112297809+arijitgh29@users.noreply.github.com>

---
+++ IntersectionOfSet.java
@@ -0,0 +1,29 @@
+import java.util.*;;
+public class IntersectionOfSet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] set1 = new int[4];
+        int[] set2 = new int[5];
+        int[] intersection = new int[0];
+        System.out.println("Set 1 :");
+        for (int i = 0; i < set1.length; i++) {
+            System.out.print((i+1)+" : ");
+            set1[i] = sc.nextInt();
+        }
+        System.out.println("Set 2 :");
+        for (int i = 0; i < set2.length; i++) {
+            System.out.print((i+1)+" : ");
+            set2[i] = sc.nextInt();
+        }
+        for (int i = 0; i < set1.length; i++) {
+            for (int j = 0; j < set2.length; j++) {
+                if(set1[i]==set2[j])
+                {
+                    intersection = Arrays.copyOf(intersection, intersection.length+1);
+                    intersection[intersection.length-1] = set1[i];
+                }
+            }
+        }
+        System.out.println("The intersection of the two sets : "+Arrays.toString(intersection));
+    }
+}
