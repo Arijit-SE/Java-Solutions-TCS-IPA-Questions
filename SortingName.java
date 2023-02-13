7ce789d4c52df551b62c59f60851731621f4c966
ARIJIT GHOSH <112297809+arijitgh29@users.noreply.github.com>
2023-02-13 09:47:15 +0530

    Add files via upload
    Signed-off-by: ARIJIT GHOSH <112297809+arijitgh29@users.noreply.github.com>

---
+++ SortingName.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class SortingName {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = new String[3];
+        String name = "";
+        for (int i = 0; i < str.length; i++) {
+            str[i] = sc.nextLine();
+        }
+        for (int i = 0; i < str.length; i++) {
+            for (int j = i+1; j < str.length; j++) {
+                if(str[i].compareTo(str[j])>0)
+                {
+                    name = str[i];
+                    str[i]=str[j];
+                    str[j]=name;
+                } 
+            }
+        }
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
+    }
+}
