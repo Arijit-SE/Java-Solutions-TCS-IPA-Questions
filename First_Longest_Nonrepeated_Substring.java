2d0a2c7ad0467d6bf500409b8d59748b777b594b
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-02-14 10:11:29 +0530

    Here we get maximum non-repeated substring from the front

---
+++ First_Longest_Nonrepeated_Substring.java
@@ -0,0 +1,32 @@
+// Arijit = Arij
+import java.util.*;
+public class First_Longest_Nonrepeated_Substring {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str = sc.nextLine();
+        String s = "";
+        int i,j,count=0;
+        for (i = 0; i < str.length(); i++) {
+            for (j = 0; j <= i; j++)
+            {
+                if(i==j)
+                {
+                    s = s + str.charAt(i);
+                }
+                else
+                {
+                    if(str.charAt(i)==str.charAt(j))
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            if(count>0)
+            {
+                break;
+            }
+        }
+        System.out.println(s);
+    }
+}
