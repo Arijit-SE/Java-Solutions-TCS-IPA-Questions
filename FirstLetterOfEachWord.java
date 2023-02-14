b20c6c90b6de1cb91af905b3d99956bc2da6e234
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-02-14 11:34:58 +0530

    Arijit Ghosh = AG

---
+++ FirstLetterOfEachWord.java
@@ -0,0 +1,16 @@
+import java.util.*;;
+public class FirstLetterOfEachWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String s = "";
+        s = s+str.charAt(0);
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i)==' ')
+            {
+                s = s + str.charAt(i+1);
+            }
+        }
+        System.out.println(s);
+    }
+}
