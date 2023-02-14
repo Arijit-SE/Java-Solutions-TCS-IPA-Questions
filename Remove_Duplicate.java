0e1373814bdacf1dda4e850ce1a63f577914c940
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-02-14 09:18:57 +0530

    Create Remove_Duplicate.java

---
+++ Remove_Duplicate.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Remove_Duplicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String ch = " ";
+        int i,j;
+        for (i = 0; i < str.length(); i++) {
+            for (j = 0; j < i; j++)
+            {
+                if(str.charAt(i)==str.charAt(j))
+                {
+                    break;
+                }
+            }
+            if(i==j)
+            {
+                ch = ch+str.charAt(i);
+            }
+        }
+        System.out.println("New string removing Duplicates : "+ch);
+    }
+}
