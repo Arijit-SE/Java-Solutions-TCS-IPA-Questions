f8bb4358ec1d4e80c54675c4460aa031a0d963f1
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-02-15 10:27:20 +0530

    Print last character of each word of a String without number and space

---
+++ LastChar.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class LastChar {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String ans = "";
+        for (int i = 0; i < s.length(); i++) 
+        {
+            if(s.charAt(i)==' ')
+            {
+                if(Character.isAlphabetic(s.charAt(i-1)))
+                {
+                    ans = ans+s.charAt(i-1);
+                }
+            }
+            else if(i==s.length()-1 && Character.isAlphabetic(s.charAt(i)))
+            {
+                ans = ans+s.charAt(i);
+            }
+        }
+        System.out.println(ans);
+    }
+}
