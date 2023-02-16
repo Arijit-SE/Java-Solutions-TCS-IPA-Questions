abeb7e90beebbcba8ecea02cf8a27f07002ea96c
arijitgh29 <arijitgh29@gmail.com>
2023-02-16 21:49:23 +0530

    Hey there! = e hr!

---
+++ Character_odd_place.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Character_odd_place {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+            if(i%2!=0)
+            {
+                System.out.print(s.charAt(i));
+            }
+        }
+        System.out.println();
+    }
+}
