d84eff45ef0056d42d9a2f7977bb514359ea1a0b
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-02-19 11:15:07 +0530

    Mising Numbers of a number set

--- test.java
+++ test.java
@@ -1,15 +1,26 @@
+/*
+ *Input : 1 2 4 8 9 11 12 14
+ *Output : 3 5 6 7 10 13
+ */
+
 import java.util.*;
 public class test {
     public static void main(String[]args)
     {
         Scanner sc= new Scanner(System.in);
-        String[] num = new String[5];
-        for(int i=0;i<5;i++)
+        int[] arr = new int[8];
+        for (int i = 0; i < arr.length; i++) 
         {
-            num[i] = sc.nextLine();
+            arr[i] = sc.nextInt();
         }
-        System.out.println(Arrays.toString(num));
-        System.out.println(String.join(",",num));
-        System.out.println();
+        int num;
+        for (int i = 0; i < arr.length-1; i++) {
+            num = arr[i];
+            while(num+1 != arr[i+1])
+            {
+                System.out.print(num+1+" ");
+                num++;
+            }
+        }
     }
 }
