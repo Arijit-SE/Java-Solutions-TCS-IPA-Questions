225608acd9686e445341b8c378ad31d2e949a8fc
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-02-25 21:46:42 +0530

    Find number of lowercase in a String

--- Count_LowerCase.java
+++ Count_LowerCase.java
@@ -1,3 +1,7 @@
+/* Find number of lower case
+ * AriJIt = 3
+ */
+
 import java.util.*;
 public class Count_LowerCase {
     public static void main(String[]args)
@@ -7,6 +11,13 @@ public class Count_LowerCase {
         String s = sc.nextLine();
         char[] arr;
         arr = s.toCharArray();
+        /* Other process to create character array
+          ******************************************
+          for(int i=0; i<s.length(); i++)
+          {
+               arr[i] = s.charAt(i);
+          }
+         */
         for(int i=0; i<arr.length;i++)
         {
             if(Character.isLowerCase(arr[i]))
