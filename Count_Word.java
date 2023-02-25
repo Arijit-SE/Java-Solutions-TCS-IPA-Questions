5f7364bf8d423cc257b61af5dbed2c378d3063c6
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-02-25 21:59:38 +0530

    Count number of words in a string

--- Count_Word.java
+++ Count_Word.java
@@ -1,3 +1,31 @@
+/* Count number of word in a string
+*Note : Always ignore the spaces if more than 1 space given between two word
+
+Sample Input : 1
+------------------------
+Kalyani Goverment Engineering College
+
+Process
+---------------------------
+[Kalyani, Goverment, Engineering, College]
+
+Output
+---------------------------
+Number of word : 4
+*************************************
+Sample Input : 2
+------------------------
+Kalyani Goverment  Engineering College
+
+Process
+---------------------------
+[Kalyani, Goverment, , Engineering, College]
+
+Output
+---------------------------
+Number of word : 4
+*/
+
 import java.util.*;;
 public class Count_Word
 {
@@ -7,7 +35,7 @@ public class Count_Word
         String s = sc.nextLine();
         String[] arr = s.split(" ");
         int count=0;
-        System.out.println(Arrays.toString(arr));
+        //System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
             if(arr[i]!="")
             {
