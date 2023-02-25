1e17b7db2dc27837dbaadf3e0647c4d5bfefced1
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-02-25 22:45:46 +0530

    Count number of even digits in a number

--- evencheck.java
+++ evencheck.java
@@ -1,14 +1,16 @@
-
+/* Count number of even digit in a number
+ * If the count greater than or equal to 3, then print "True" otherwise "False"
+ */
 import java.util.*;
-
 public class evencheck
 {
     public static void main(String[] args) 
     {
         int n;
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
         int count = 0;
+        /*
+        n = sc.nextInt();
         while(n>0)
         {
             int rem = n%10;
@@ -16,6 +18,15 @@ public class evencheck
                 count++;
             }
             n = n/10;
+        }
+        */
+        String num = sc.nextLine();
+        for (int i = 0; i < num.length(); i++) 
+        {
+            if(Integer.parseInt(String.valueOf(num.charAt(i)))%2==0)
+            {
+                count++;
+            }
         }
         if(count>=3)
         {
