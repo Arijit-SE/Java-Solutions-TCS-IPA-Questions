5c1a7fa9076f2d7dda16e4d69f5d5d38685900af
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-02-25 21:03:54 +0530

    Average between two numbers

--- avg.java
+++ avg.java
@@ -1,11 +1,27 @@
-package Java;
+/* Find average of a number array between two limit numbers (except the two numbers)
+ * Input
+ * ---------------
+ *  5
+    1 
+    2
+    3
+    4
+    5
+ *  1
+ *  4
+ * 
+ * Output
+ * ---------------
+ * 2
+ */
 import java.util.*;
 public class avg 
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[7];
-        for(int i=0; i<7; i++)
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++)
         {
             arr[i] = sc.nextInt();
         }
@@ -13,22 +29,23 @@ public class avg
         int up = sc.nextInt();
 
         int lowerIndex = index(arr, low);
-        if(lowerIndex==0)
-        {
-            System.out.println("Index not found");
-        }
+        
         int upperIndex = index(arr, up);
         if(upperIndex==0)
         {
             System.out.println("Index not found");
         }
         int sum=0, count=0;
-        for(int i=lowerIndex+1; i<upperIndex; i++)
+        if(upperIndex>0)
         {
-            sum = sum+arr[i];
-            count++;
+            for(int i=lowerIndex+1; i<upperIndex; i++)
+            {
+                sum = sum+arr[i];
+                count++;
+            }
+            System.out.println(sum/count);
         }
-        System.out.println(sum/count);
+        
     }
 
     public static int index(int[] arr, int num)
