b7f48ce52f38dad16bf6ab8b463c0ba4eaff49b7
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-03-01 08:29:15 +0530

    Reverse of a string

--- ReverseString.java
+++ ReverseString.java
@@ -1,10 +1,24 @@
+/* Reverse of a string
+ * Arijit = tijirA
+ */
 import java.util.*;
 public class ReverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
+
+        /* One method
+        --------------------------------------
         for (int i = s.length()-1; i>=0; i--) {
             System.out.print(s.charAt(i)+"");
         }
+        */
+        // Another method
+        //-----------------------------------
+        String n = "";
+        for (int i = 0; i < s.length(); i++) {
+            n = s.charAt(i)+n;
+        }
+        System.out.println(n);
     }
 }
