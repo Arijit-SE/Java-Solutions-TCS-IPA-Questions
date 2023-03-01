8fce6947000971aaab3782f0c78fb382e67e4253
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-03-01 08:41:39 +0530

    Sum of the numbers break by words

--- SumOfNum.java
+++ SumOfNum.java
@@ -1,11 +1,23 @@
-package Java;
+/* Input : 123Arijit1998Arijit10
+
+ * Process :
+ * -----------
+ * Array : [123, Arijit, 1998, Arijit, 10]
+ * Now you should sum up the position of that array where numbers are stored.
+ * 123 + 1998 = 2121
+ * 
+ * Output :
+ * --------------
+ * 2131
+ */
+
 import java.util.*;
 public class SumOfNum 
 {
     public static void main(String[] args) 
     {
         int sum=0,num=0;
-        String str = "123Arijit1998Arijit";
+        String str = "123Arijit1998Arijit10";
         for (int i = 0; i < str.length(); i++) {
             if(Character.isDigit(str.charAt(i)))
             {
