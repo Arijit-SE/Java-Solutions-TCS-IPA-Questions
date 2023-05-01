214ffdc867414d2eac0d2af6fb2e425f27ae7b1f
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-05-01 09:19:00 +0530

    Count the number of words that start with a capital letter

---
+++ Find_UpperCase.java
@@ -0,0 +1,18 @@
+/*Given a sentence, count the number of words that start with a capital letter.
+
+For example, in the sentence "The quick Brown Fox jumps over the lazy dog", there are three words that start with a 
+capital letter: "The", "Brown", and "Fox". */
+
+import java.util.*;
+public class Find_UpperCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.nextLine().split(" ");
+        for (int i = 0; i < s.length; i++) {
+            if(s[i].charAt(0)>='A' && s[i].charAt(0)<='Z')
+            {
+                System.out.println(s[i]);
+            }
+        }
+    }
+}
