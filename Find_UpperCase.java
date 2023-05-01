958021cecb38d9a5735d511c8a11dbbddea2f2c9
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-05-01 09:23:05 +0530

    Find words that start with a capital letter

--- Find_UpperCase.java
+++ Find_UpperCase.java
@@ -8,6 +8,7 @@ public class Find_UpperCase {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] s = sc.nextLine().split(" ");
+        System.out.println("The words starts with capital letter: ");
         for (int i = 0; i < s.length; i++) {
             if(s[i].charAt(0)>='A' && s[i].charAt(0)<='Z')
             {
