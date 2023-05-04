af5e4564ce2a525fb2d840d7e8c76e9fd506cf7e
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-05-04 19:55:18 +0530

    Remove two consecutive word

---
+++ Remove2ConsecutiveLetter.java
@@ -0,0 +1,33 @@
+/*
+ * You are given a string S consisting of lowercase letters. Your task is to remove all the consecutive duplicates from
+ * the string and output the result. For example, if the input string is "abbcddeff", the output should be "abcdef".
+ * 
+ * Input: abbcddeff
+ * Output: abcdef
+
+ */
+
+import java.util.*;
+public class Remove2ConsecutiveLetter 
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String result = removeConsecutiveDuplicates(s);
+        System.out.println(result);
+    }
+
+    private static String removeConsecutiveDuplicates(String s) {
+        String str = "";
+        char ch = '\0';
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)!=ch)
+            {
+                ch = s.charAt(i);
+                str += ch;
+            }
+        }
+        return str;
+    }
+}
+
