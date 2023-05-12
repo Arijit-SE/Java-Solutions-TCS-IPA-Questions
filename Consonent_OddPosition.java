235c0c65c584d280e95c2fc037d3f2b976eaeb3c
ARIJIT GHOSH <arijitgh29@gmail.com>
2023-05-12 19:26:26 +0530

    Find the characters in odd position of a string which are consonent

---
+++ Consonent_OddPosition.java
@@ -0,0 +1,47 @@
+/* Find the characters in odd position of a string which are consonent
+ * 
+ * Input: Arijit Ghosh
+ * Output: hs
+ */
+
+import java.util.Scanner;
+
+public class Consonent_OddPosition {
+    /*
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().toLowerCase();
+        String v = "aeiou";
+        for (int i = 0; i < s.length(); i++) {
+            if((i+1)%2!=0 && !v.contains(Character.toString(s.charAt(i))) && s.charAt(i)!=' ')
+            {
+                System.out.print(s.charAt(i));
+            }
+        }    
+    }
+     */
+
+    // Another Solution
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().toLowerCase();
+        char[] ch = {'a', 'e', 'i', 'o', 'u'};
+        for (int i = 0; i < s.length(); i++) {
+            boolean test = true;
+            for (int j = 0; j < ch.length; j++) {
+                if((i+1)%2==0 || s.charAt(i)==ch[j] || s.charAt(i)==' ')
+                {
+                    test = false;
+                }
+            }
+            if(test == true)
+            {
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+}
+
+
